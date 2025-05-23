f0e57858164cb85a2e8e87c3d44a4bfd17eae9cf
Rosario <RMC>
2025-05-23 14:23:28 -0700

    Renamed & Reformatted

--- src/main/java/DeliciousSandos.java
+++ src/main/java/DeliciousSandos.java
@@ -1,6 +1,6 @@
 import menuScreens.MainMenu;
 
-public class Main {
+public class DeliciousSandos {
     public static void main(String[] args) {
         MainMenu main = new MainMenu();
         main.display();
